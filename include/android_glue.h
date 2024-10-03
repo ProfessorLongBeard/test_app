6a62d605bceecc2fda37c6d5d561a716fdeca637
ProfessorLongBeard <austingarrick752@gmail.com>
2024-10-02 19:00:47 -0700

    Misc

---
+++ include/android_glue.h
@@ -0,0 +1,13 @@
+#ifndef ANDROID_GLUE_H
+#define ANDROID_GLUE_H
+
+#include <stdint.h>
+
+#include <jni.h>
+
+#include <android/log.h>
+#include <android/looper.h>
+#include <android/configuration.h>
+#include <android/native_activity.h>
+
+#endif
---
+++ src/android_glue.c
@@ -0,0 +1,8 @@
+#include <android_glue.h>
+
+
+
+
+JNIEXPORT void ANativeActivity_onCreate(ANativeActivity *ac, void *saved_state, size_t saved_state_size) {
+    
+}
